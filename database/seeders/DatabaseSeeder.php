f37b7128441d55234bc82da1067c3e3323e51a16
Quadrubo <71718414+Quadrubo@users.noreply.github.com>
2023-01-12 11:17:39 +0100

    add seeders to production seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(RoleSeeder::class);
+        $this->call(PermissionSeeder::class);
+        $this->call(RoleHasPermissionsSeeder::class);
+
         $this->call(PlatformSeeder::class);
     }
 }
