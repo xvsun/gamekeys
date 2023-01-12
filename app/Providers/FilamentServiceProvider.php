6472460de7c5323b0b5dac9b65801f006b018a9b
Quadrubo <71718414+Quadrubo@users.noreply.github.com>
2023-01-12 11:47:33 +0100

    added filament authorization resources

---
+++ app/Providers/FilamentServiceProvider.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Providers;
+
+use Filament\Facades\Filament;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\ServiceProvider;
+
+class FilamentServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Filament::serving(function () {
+            \Filament\Support\Components\ViewComponent::macro('localize', function (?string $translatorPrefix = '', ?bool $label = true, ?bool $helper = true, ?bool $hint = true, ?bool $datetime = false, ?array $labelAttributes = [], ?array $helperAttributes = [], ?array $hintAttributes = []) {
+                if (! str_ends_with($translatorPrefix, '.')) {
+                    $translatorPrefix = $translatorPrefix.'.';
+                }
+
+                if ($label) {
+                    $this->label(__($translatorPrefix.'label', $labelAttributes));
+                } else {
+                    $this->label = '';
+                }
+
+                if ($helper) {
+                    $this->helperText(__($translatorPrefix.'helper', $helperAttributes));
+                } else {
+                    $this->helper = '';
+                }
+
+                if ($hint) {
+                    $this->hint(__($translatorPrefix.'hint', $hintAttributes));
+                } else {
+                    $this->hint = '';
+                }
+
+                /*if ($datetime) {
+                    if ($this instanceof \Filament\Forms\Components\TimePicker) {
+                        $this->displayFormat(__('app.preferences.time.'.Auth::user()->preference->time_format.'.carbon'));
+                    } elseif ($this instanceof \Filament\Forms\Components\DatePicker) {
+                        $this->displayFormat(__('app.preferences.date.'.Auth::user()->preference->date_format.'.carbon'));
+                    } elseif ($this instanceof \Filament\Forms\Components\DateTimePicker) {
+                        $this->displayFormat(__('app.preferences.datetime.'.Auth::user()->preference->datetime_format.'.carbon'));
+                    }
+                }*/
+
+                return $this;
+            });
+        });
+    }
+}
